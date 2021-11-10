02d859bf67d07bedbe19a16b77532f631fb74e59
Kobi Yamin <kobi2132@gmail.com>
2021-11-10 21:02:24 +0200

    1011-2102

--- js/apps/keep/cmps/note-edit-bar.cmp.js
+++ js/apps/keep/cmps/note-edit-bar.cmp.js
@@ -5,7 +5,16 @@ export default {
     template: `
         <div class="note-edit-bar">
             <button class="edit-pin edit-btns"></button>
-            <button class="edit-color edit-btns"></button>
+            <div class="edit-color-section">
+                <button class="edit-color edit-btns"></button>
+                <div class="color-plette">
+                    <div class="color-select color-red" @click="changeColor('#f28b82')"></div>
+                    <div class="color-select color-orange"></div>
+                    <div class="color-select"></div>
+                    <div class="color-select"></div>
+                    <div class="color-select"></div>
+                </div>
+            </div>
             <button class="edit-send edit-btns"></button>
             <button class="edit-edit edit-btns"></button>
             <button class="edit-delete edit-btns" @click="remove(note.id)"></button>
@@ -16,6 +25,10 @@ export default {
     methods:{
         remove(noteId){
             this.$emit('remove', noteId)
+        },
+
+        changeColor(color){
+console.log(color);
         }
     },
 
