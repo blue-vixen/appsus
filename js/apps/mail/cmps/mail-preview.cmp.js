f4bd9032e6615369d258d9dea621a2bf9d66859d
Adi Levy-Alalouf <blue.vixen@gmail.com>
2021-11-11 20:44:12 +0200

    final commit before second delivery

--- js/apps/mail/cmps/mail-preview.cmp.js
+++ js/apps/mail/cmps/mail-preview.cmp.js
@@ -12,11 +12,11 @@ export default {
             <li class="bold">{{email.to || email.from}}</li>
             <li><span class="bold">{{email.subject}} -</span> {{email.body}}</li>
             <li>{{renderDate}}</li>
-            <div class="actions">
+            <!-- <div class="actions">
                 <button title="delete" class="btn-delete" @click="remove(email.id)"></button>
                 <button title="expand" class="btn-expand" @click="redirectToMail(email.id)"></button>
                 <button title="save to note" class="btn-note"></button>
-            </div>    
+            </div>     -->
         </ul>
 
     `,
